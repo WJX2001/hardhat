ae4e7d3bc60e9cd7639ca713a270c59b46c6e85d
WJX2001 <1758807220@qq.com>
2024-10-22 11:13:17 +0800

    feat: 通过脚本对合约进行交互

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,21 +1,10 @@
-
-/**
- * init 2 accounts
- * fund contract with first account
- * check balance of contract
- * fund contract with second accout
- * check balance of contract
- * check mapping  fundersToAmount
- */
-
-
 const { ethers } = require('hardhat')
 require("@chainlink/env-enc").config()
 async function main () {
   // create factory
   const fundMeFactory = await ethers.getContractFactory('FundMe')
   // deploy contract from factory
-  const fundMe = await fundMeFactory.deploy(10)
+  const fundMe = await fundMeFactory.deploy(300)
   await fundMe.waitForDeployment()
   console.log(
     'contract has been deployed successfully, contract address is' +
@@ -27,11 +16,44 @@ async function main () {
   if (hre.network.config.chainId == 11155111 && process.env.ETH_VERIFY_API_KEY) {
     console.log('waiting for 5 confirmations...')
     await fundMe.deploymentTransaction().wait()
-    await verifyFundMe(fundMe.target, [10])
+    await verifyFundMe(fundMe.target, [300])
   } else {
     console.log("verification skipped")
   }
 
+  /**
+ * init 2 accounts
+ * fund contract with first account
+ * check balance of contract
+ * fund contract with second accout
+ * check balance of contract
+ * check mapping  fundersToAmount
+ */
+
+  // init 2 accounts
+  const [firstAccount, secondAccount] = await ethers.getSigners()
+
+  // fund contract with first account
+  const fundTx = await fundMe.fund({ value: ethers.parseEther("0.1") })
+  await fundTx.wait()
+
+  // check balance of contract
+  const balanceOfContract = await ethers.provider.getBalance(fundMe.target)
+  console.log(`balance of contract is ${balanceOfContract}`)
+
+  // fund contract with second accout
+  const fundTxWithSecondAccount = await fundMe.connect(secondAccount).fund({ value: ethers.parseEther("0.1") })
+  await fundTxWithSecondAccount.wait()
+
+  // check balance of contract
+  const balanceOfContractAfterSecondFund = await ethers.provider.getBalance(fundMe.target)
+  console.log(`balance of contract is ${balanceOfContractAfterSecondFund}`)
+
+  // check mapping fundersToAmount
+  const firstAccountBalanceInFundMe = await fundMe.funderToAmount(firstAccount.address)
+  const secondAccountBalanceInFundMe = await fundMe.funderToAmount(secondAccount.address)
+  console.log(`Balance of first account ${firstAccount.address} is ${firstAccountBalanceInFundMe}`)
+  console.log(`Balance of second account ${secondAccount.address} is ${secondAccountBalanceInFundMe}`)
 }
 
 async function verifyFundMe (fundMeAddr, args) {
